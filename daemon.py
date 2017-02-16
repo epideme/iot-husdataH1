bd3ffed0ab5e123e4b8aee8ff67960567dc71fd1
epideme <tony.nilsson@epideme.com>
2017-02-16 14:22:18 +0000

    First embryo

--- daemon.py
+++ daemon.py
@@ -1,1 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+import time
+import serial
+      
+  
+ser = serial.Serial(
+          
+  port='/dev/ttyAMA0',
+  baudrate = 19200,
+  timeout=10
+)
+counter=0
+
+ser.write('!\r\n')
+time.sleep(2)
+ser.write('XP\r\n')
+time.sleep(2)
+  
+while 1:
+  x=ser.readline()
+  print x
+  print "."
+
