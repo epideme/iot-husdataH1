9e2d627d99d75435aa2f1f614f75191dc6fd980c
epideme <tony.nilsson@epideme.com>
2017-02-17 19:19:50 +0000

    This might work!

--- daemon.py
+++ daemon.py
@@ -4,7 +4,10 @@ import time
 import serial
 import re
 import urllib2
-  
+
+# Server name of iot server
+servername="homepi"
+
 ser = serial.Serial(
   port='/dev/serial0',
   baudrate = 19200,
@@ -20,13 +23,18 @@ time.sleep(2)
 ser.flushInput()  
 while 1:
   line=ser.readline()
+  # line="XP00001234 005 Brine in/Evaporator (11.7c)"
   if line:
-    splitline=line.split(' ')
-    label=splitline[2]
-    value = re.sub('(c)', '', splitline[3])
+    splitline=line.split(' (')
+    label=splitline[0].split(' ')
+    label='+'.join(label[2:])
+    label=re.sub('/', '+', label)
+    value = re.sub('[c\) ]', '', splitline[1])
     print "Label:", label
     print "Value:", value
-    urllib2.urlopen("http://localhost/iot/iotstore.php?id=" + label + "&set=" + value).read()
+    url="http://" + servername + "/iot/iotstore.php?id=HeatPump+" + label + "&set=" + value
+    print url
+    urllib2.urlopen(url).read()
   else:
     print "Waiting..."
 
