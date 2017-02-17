1fd2bdc4a3fa9b4a0e022965ecbba8989dce83ac
epideme <tony.nilsson@epideme.com>
2017-02-17 14:09:55 +0000

    Starting to get somewhere...

--- daemon.py
+++ daemon.py
@@ -7,23 +7,27 @@ import re
   
 ser = serial.Serial(
           
-  port='/dev/ttyAMA0',
+  port='/dev/serial0',
   baudrate = 19200,
   timeout=10
 )
 counter=0
 
-ser.write('!\r\n')
+print "Sending reset"
+ser.write("!\r\n")
 time.sleep(2)
-ser.write('XP\r\n')
+print "Toggle readable output"
+ser.write("XP\r\n")
 time.sleep(2)
-  
+ser.flushInput()  
 while 1:
   line=ser.readline()
-  splitline=line.split(' ')
-  label=splitline[2]
-  value = re.sub('(c)', '', splitline[3])
-  print "Label:", label
-  print "Value:", value
-  print "."
+  if line:
+    splitline=line.split(' ')
+    label=splitline[2]
+    value = re.sub('(c)', '', splitline[3])
+    print "Label:", label
+    print "Value:", value
+  else:
+    print "Waiting..."
 
