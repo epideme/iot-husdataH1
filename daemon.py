95c635cd153569d5b8850a470760987143403ce6
epideme <tony.nilsson@epideme.com>
2017-02-23 12:30:07 +0000

    Big cleanup and changed send command Verification of send command now properly done. Also command file now in format "XXXX nn" where XXXX is heatpump Hex register and nn is digit.

--- daemon.py
+++ daemon.py
@@ -18,48 +18,39 @@ ser = serial.Serial(
 
 print "Sending reset"
 ser.write("!\r\n")
-time.sleep(2)
+print "Waiting for H1 to settle"
 line=ser.readline()
-line=re.sub('[\n\r]', '', line)
-print line
 while line:
   line=ser.readline()
-  line=re.sub('[\n\r]', '', line)
-  print line
 
 print "Toggle readable output"
 ser.write("XP\r\n")
+print  "Waiting for H1 to settle"
 line=ser.readline()
-line=re.sub('[\n\r]', '', line)
-print line
 while line:
   line=ser.readline()
-  line=re.sub('[\n\r]', '', line)
-  print line
+
 print "Toggle heatpump specific output"
 ser.write("XS\r\n")
+print "Waiting for H1 to settle"
 line=ser.readline()
-line=re.sub('[\n\r]', '', line)
-print line
 while line:
   line=ser.readline()
-  line=re.sub('[\n\r]', '', line)
-  print line
+
 print "Toggle scheduled full output"
 ser.write("XM\r\n")
+print "Waiting for H1 to settle"
 line=ser.readline()
-line=re.sub('[\n\r]', '', line)
-print line
 while line:
   line=ser.readline()
-  line=re.sub('[\n\r]', '', line)
-  print line
+
+print "Start collection of data..."
+
 while 1:
   line=ser.readline()
   line=re.sub('[\n\r]', '', line)
-  print line
-  # line="XP00001234 005 Brine in/Evaporator (11.7c)"
   if line:
+    print line
     splitline=line.split(' (')
     labels=splitline[0].split(' ')
     label=labels[4:]
@@ -70,21 +61,34 @@ while 1:
       label=re.sub(',', '', label)
       value = re.sub('[hpcd\) %]', '', splitline[1])
       if value:
-        #print "Label:", label
-        #print "Value:", value
         url="http://" + servername + "/iot/iotstore.php?id=HeatPump+" + label + "&set=" + value
-        #print url
         urllib2.urlopen(url).read()
   else:
-    print "Waiting..."
     if os.path.exists("/tmp/hpcommand.txt"):
       file = open("/tmp/hpcommand.txt", "r")
-      sendcommand=file.read(10) + "\r"
-      print "Writing command: " + sendcommand
-      ser.write(sendcommand)
-      line=ser.readline()
-      line=re.sub('[\n\r]', '', line)
-      print line
+      filedata=file.read().split(' ')
+      dataregister=filedata[0]
+      datavalue=format(int(float(filedata[1]))*10, '04X')
+      sendcommand="XW" + dataregister + datavalue + "\r"
+      expectcommand="XR" + dataregister + datavalue
+      waitingreply=1
+      while waitingreply:
+        while line<>"XW01":
+          print "Writing command: " + sendcommand
+          ser.flushOutput()
+          ser.flushInput()
+          ser.write(sendcommand)
+          line=ser.readline()
+          line=re.sub('[\n\r]', '', line)
+          print line
+        while (line and waitingreply):
+          line=ser.readline()
+          line=re.sub('[\n\r]', '', line)
+          print "Received line " + line[:10] + " (Wanted " + expectcommand + ")"
+          if line[:10] == expectcommand:
+            waitingreply=0
+            print "Received reply " + line[:10]
+
       file.close()
       call(["rm", "/tmp/hpcommand.txt"])
 
